7792b297c9757ed6a45f829e655706a9fe89b039
Matt Mannings <37386150+Jago971@users.noreply.github.com>
2025-01-13 14:56:25 +0000

    hero name, hero text, hero icons - included and animated

--- script.js
+++ script.js
@@ -14,19 +14,36 @@ function expandSections(width) {
   }
 }
 
-//text
+//text effects
 function fadeDownTextEffect(parent, sentence) {
-  sentence.split("").forEach((character, index) => {
-    const element = document.createElement("span");
+  let sentenceCharIndex = 0;
+  sentence.split(" ").forEach((word, wordIndex) => {
+    const wordDiv = document.createElement("div");
+    wordDiv.style.display = "inline-block";
+    wordDiv.style.whiteSpace = "nowrap";
 
-    if (specialCharRegex.test(character)) {
-      element.classList.add("special-character");
+    word.split("").forEach((character) => {
+      const charSpan = document.createElement("span");
+
+      if (specialCharRegex.test(character)) {
+        charSpan.classList.add("special-character");
+      }
+
+      charSpan.textContent = character;
+      charSpan.style.animationDelay = `${sentenceCharIndex * 0.04}s`;
+      charSpan.classList.add("hero-name-character");
+      wordDiv.appendChild(charSpan);
+      sentenceCharIndex++;
+    });
+
+    parent.appendChild(wordDiv);
+
+    if (wordIndex < sentence.split(" ").length - 1) {
+      const space = document.createElement("span");
+      space.textContent = "\u00A0";
+      parent.appendChild(space);
+      sentenceCharIndex++;
     }
-
-    element.textContent = character === " " ? "\u00A0" : character;
-    element.style.animationDelay = `${index * 0.03}s`;
-    element.classList.add("hero-name-character");
-    parent.appendChild(element);
   });
 }
 
@@ -51,6 +68,14 @@ function typeWriterTextEffect(parent, sentence) {
   }
 }
 
+//icons
+function fadeIconsIn() {
+  icons.forEach((icon, index) => {
+    icon.style.animation = "fadeDownAnim 0.5s ease-in-out forwards"
+    icon.style.animationDelay = `${index * 0.1}s`;
+  });
+}
+
 //execution
 const wrap = document.querySelector(".wrap");
 const sections = document.querySelectorAll(".wrap div");
@@ -59,6 +84,8 @@ const heroTextGrid = document.querySelector(".hero-text-grid");
 const heroText = document.querySelectorAll(".hero-text p");
 const caret = document.querySelector(".caret");
 const specialCharRegex = /[!@#$%^&*(),.?":{}|<>'-]/;
+const iconGrid = document.querySelector(".icon-grid");
+const icons = document.querySelectorAll(".icons img");
 
 const heroInputs = [
   "Hey, I'm Matt.",
@@ -68,14 +95,20 @@ const heroInputs = [
 fadeDownTextEffect(heroName, heroInputs[0]);
 
 setTimeout(() => {
-  heroText[0].innerHTML = heroInputs[1] + "<span style='font-weight: bold;'>|</span>";
+  heroText[0].innerHTML =
+    heroInputs[1] + "<span style='font-weight: bold;'>|</span>";
   heroTextGrid.style.gridTemplateRows = "1fr";
-}, 2000);
+  iconGrid.style.gridTemplateRows = "1fr";
+}, 1250);
 
 setTimeout(() => {
   caret.style.display = "inline-block";
-});
+}, 2000);
 
 setTimeout(() => {
   typeWriterTextEffect(heroText[1], heroInputs[1]);
-}, 3500);
+}, 3000);
+
+setTimeout(() => {
+  fadeIconsIn()
+}, 7500)
