bfbf2b419188155595f644605ead021e35fdfe57
Matt Mannings <37386150+Jago971@users.noreply.github.com>
2025-01-15 17:45:44 +0000

    both text anims working, hero expand and slide over working

--- script.js
+++ script.js
@@ -36,7 +36,26 @@ function fadeDownTextEffect(parent, sentence) {
   });
 }
 
-function exapndHeroSection() {
+function typeWriterTextEffect(parent, sentence) {
+  if (sentence.length > 0) {
+    const character = sentence.split("").shift();
+    const caret = parent.querySelector(".caret");
+    if (specialCharRegex.test(character)) {
+      const element = document.createElement("span");
+      element.textContent = character;
+      element.classList.add("special-character");
+      parent.insertBefore(element, caret);
+    } else {
+      caret.insertAdjacentText("beforebegin", character)
+    }
+
+    setTimeout(() => {
+      typeWriterTextEffect(parent, sentence.slice(1));
+    }, 60);
+  }
+}
+
+function expandHeroSection() {
   const grids = sections[1].querySelectorAll(".grid-container")
   grids.forEach(grid => {
     grid.style.gridTemplateRows = "1fr"
@@ -45,11 +64,30 @@ function exapndHeroSection() {
 
 const sections = document.querySelectorAll("section")
 const specialCharRegex = /[!@#$%^&*(),.?":{}|<>'-]/;
+const heroName = document.querySelector(".hero-name")
+const heroText = document.querySelector(".hero-text")
 const heroInputs = [
   "Hey, I'm Matt.",
   "I'm developing my expertise in full-stack development.",
 ];
+const caret = document.querySelector(".caret");
 
-fadeDownTextEffect(sections[1].querySelector("h1"), heroInputs[0])
-setTimeout(() => {exapndHeroSection()},2000)
-setTimeout(() => {expandSections()},5000)
+fadeDownTextEffect(heroName, heroInputs[0])
+
+setTimeout(() => {
+  expandHeroSection()
+  const text = document.createElement("h2")
+  text.innerHTML =
+    heroInputs[1] + "<span style='font-weight: bold;'>|</span>";
+  heroText.appendChild(text)
+}, 1250);
+
+setTimeout(() => {
+  caret.style.display = "inline-block";
+}, 2000);
+
+setTimeout(() => {
+  typeWriterTextEffect(heroText.querySelector("h2"), heroInputs[1]);
+}, 3000);
+
+setTimeout(() => {expandSections()},6750)
