a0361f04a9014349f55fae6413d32b1955076e3c
Matt Mannings <37386150+Jago971@users.noreply.github.com>
2025-01-15 13:15:10 +0000

    restart

--- script.js
+++ script.js
@@ -1,28 +1,36 @@
-//sections
+// //sections
+// function expandSections() {
+//   wrapper.style.width = "100%"
+//   changeGridOrientation(window.innerWidth)
+//   sections.forEach((section) => {
+//     section.style.display = "flex";
+//   });
+// }
+
+// function changeGridOrientation(width) {
+//   if (width > 768) {
+//     wrapper.style.gridTemplateColumns = "2fr 1fr 2fr";
+//     wrapper.style.gridTemplateRows = "1fr";
+//   } else {
+//     wrapper.style.gridTemplateColumns = "1fr";
+//     wrapper.style.gridTemplateRows = "1fr 2fr 1fr";
+//   }
+// }
+
+// const wrapper = document.querySelector(".wrapper")
+// const sections = document.querySelectorAll("section")
+// const heroSection = document.querySelector(".hero-section")
+
+// setTimeout(() => {expandSections()},2000)
+
+// window.addEventListener("resize", function() {
+//   changeGridOrientation(window.innerWidth);
+// });
+
 function expandSections() {
-  wrapper.style.width = "100%"
-  changeGridOrientation(window.innerWidth)
-  sections.forEach((section) => {
-    section.style.display = "flex";
-  });
+  sections[0].style.flex = "0 0 0"
 }
 
-function changeGridOrientation(width) {
-  if (width > 768) {
-    wrapper.style.gridTemplateColumns = "2fr 1fr 2fr";
-    wrapper.style.gridTemplateRows = "1fr";
-  } else {
-    wrapper.style.gridTemplateColumns = "1fr";
-    wrapper.style.gridTemplateRows = "1fr 2fr 1fr";
-  }
-}
-
-const wrapper = document.querySelector(".wrapper")
 const sections = document.querySelectorAll("section")
-const heroSection = document.querySelector(".hero-section")
 
 setTimeout(() => {expandSections()},2000)
-
-window.addEventListener("resize", function() {
-  changeGridOrientation(window.innerWidth);
-});
