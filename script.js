bff5302545e2ab669064c5a94c3795cee8507428
Matt Mannings <37386150+Jago971@users.noreply.github.com>
2025-01-15 10:57:28 +0000

    mobile layout needs finessing

--- script.js
+++ script.js
@@ -1,27 +1,27 @@
 //sections
 function expandSections() {
+  wrapper.style.width = "100%"
   changeGridOrientation(window.innerWidth)
-  setTimeout (() => {
-    sections.forEach((section) => {
-      section.style.display = "flex";
-    });
-  }, 2000)
+  sections.forEach((section) => {
+    section.style.display = "flex";
+  });
 }
 
 function changeGridOrientation(width) {
   if (width > 768) {
-    body.style.gridTemplateColumns = "2fr 1fr 2fr";
-    body.style.gridTemplateRows = "1fr";
+    wrapper.style.gridTemplateColumns = "2fr 1fr 2fr";
+    wrapper.style.gridTemplateRows = "1fr";
   } else {
-    body.style.gridTemplateColumns = "1fr";
-    body.style.gridTemplateRows = "1fr 2fr 1fr";
+    wrapper.style.gridTemplateColumns = "1fr";
+    wrapper.style.gridTemplateRows = "1fr 2fr 1fr";
   }
 }
 
-const body = document.querySelector("body")
+const wrapper = document.querySelector(".wrapper")
 const sections = document.querySelectorAll("section")
+const heroSection = document.querySelector(".hero-section")
 
-expandSections()
+setTimeout(() => {expandSections()},2000)
 
 window.addEventListener("resize", function() {
   changeGridOrientation(window.innerWidth);
