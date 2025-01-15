5cb192c8c155d3dab4b4c955d5f2e87161db28db
Matt Mannings <37386150+Jago971@users.noreply.github.com>
2025-01-15 13:32:24 +0000

    textAnims

--- script.js
+++ script.js
@@ -1,36 +1,42 @@
-// //sections
-// function expandSections() {
-//   wrapper.style.width = "100%"
-//   changeGridOrientation(window.innerWidth)
-//   sections.forEach((section) => {
-//     section.style.display = "flex";
-//   });
-// }
-
-// function changeGridOrientation(width) {
-//   if (width > 768) {
-//     wrapper.style.gridTemplateColumns = "2fr 1fr 2fr";
-//     wrapper.style.gridTemplateRows = "1fr";
-//   } else {
-//     wrapper.style.gridTemplateColumns = "1fr";
-//     wrapper.style.gridTemplateRows = "1fr 2fr 1fr";
-//   }
-// }
-
-// const wrapper = document.querySelector(".wrapper")
-// const sections = document.querySelectorAll("section")
-// const heroSection = document.querySelector(".hero-section")
-
-// setTimeout(() => {expandSections()},2000)
-
-// window.addEventListener("resize", function() {
-//   changeGridOrientation(window.innerWidth);
-// });
-
+//layout
 function expandSections() {
   sections[0].style.flex = "0 0 0"
 }
 
+//text effects
+function fadeDownTextEffect(parent, sentence) {
+  let sentenceCharIndex = 0;
+  sentence.split(" ").forEach((word, wordIndex) => {
+    const wordDiv = document.createElement("div");
+    wordDiv.style.display = "inline-block";
+    wordDiv.style.whiteSpace = "nowrap";
+
+    word.split("").forEach((character) => {
+      const charSpan = document.createElement("span");
+
+      if (specialCharRegex.test(character)) {
+        charSpan.classList.add("special-character");
+      }
+
+      charSpan.textContent = character;
+      charSpan.style.animationDelay = `${sentenceCharIndex * 0.04}s`;
+      charSpan.classList.add("hero-name-character");
+      wordDiv.appendChild(charSpan);
+      sentenceCharIndex++;
+    });
+
+    parent.appendChild(wordDiv);
+
+    if (wordIndex < sentence.split(" ").length - 1) {
+      const space = document.createElement("span");
+      space.textContent = "\u00A0";
+      parent.appendChild(space);
+      sentenceCharIndex++;
+    }
+  });
+}
+
 const sections = document.querySelectorAll("section")
 
-setTimeout(() => {expandSections()},2000)
+fadeDownTextEffect()
+setTimeout(() => {expandSections()},5000)
